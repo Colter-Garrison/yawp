6c680b359f3026244dacb03390c521b3a3bbe4cc
Colter Garrison <102547626+Colter-Garrison@users.noreply.github.com>
2022-08-15 16:38:23 -0700

    first route passing POST /users creates a new user

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,15 +1,32 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
 
-describe('backend-express-template routes', () => {
+const testUser = {
+  firstName: 'Tiesto',
+  lastName: 'Testerson',
+  email: 'testemail@test.com',
+  password: 'asdf',
+};
+
+describe('yawp POST, GET, and DELETE route tests', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+
+  it('POST should create a new user', async () => {
+    const resp = await request(app).post('/api/v1/users').send(testUser);
+    const { firstName, lastName, email } = testUser;
+
+    expect(resp.body).toEqual({
+      id: expect.any(String),
+      firstName,
+      lastName,
+      email,
+    });
   });
+  
   afterAll(() => {
     pool.end();
   });
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,27 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS restaurants;
+DROP TABLE IF EXISTS reviews;
+
+CREATE TABLE users (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  first_name TEXT NOT NULL,
+  last_name TEXT NOT NULL,
+  email TEXT NOT NULL,
+  password_hash TEXT NOT NULL
+);
+
+CREATE TABLE restaurants (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  name TEXT NOT NULL,
+  cuisine TEXT NOT NULL
+);
+
+CREATE TABLE reviews (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  stars INT NOT NULL,
+  detail TEXT NOT NULL,
+  restaurant_id BIGINT NOT NULL,
+  user_id BIGINT NOT NULL
+);
