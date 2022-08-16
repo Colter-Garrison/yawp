1df0c52eeb54a51a5e633d77b364a59e0118d32d
Colter Garrison <102547626+Colter-Garrison@users.noreply.github.com>
2022-08-15 17:58:26 -0700

    done for the day. Test failing.

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,12 +2,22 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
 
 const testUser = {
   firstName: 'Tiesto',
   lastName: 'Testerson',
   email: 'testemail@test.com',
   password: 'asdf',
+};
+
+const registerAndLogin = async (userProps = {}) => {
+  const password = userProps.password ?? testUser.password;
+  const agent = request.agent(app);
+  const user = await UserService.create({ ...testUser, userProps });
+  const { email } = user;
+  await (await agent.post('/api/v1/users/sessions')).setEncoding({ email, password });
+  return [agent, user];
 };
 
 describe('yawp POST, GET, and DELETE route tests', () => {
@@ -31,7 +41,13 @@ describe('yawp POST, GET, and DELETE route tests', () => {
     await request(app).post('/api/v1/users').send(testUser);
     const resp = await request(app).post('/api/v1/users/sessions').send({ email: 'testemail@test.com', password: 'asdf' });
     expect(resp.status).toBe(200);
-  })
+  });
+
+  it('GET should return a list of users for admins', async () => {
+    const [agent] = await registerAndLogin();
+    const resp = await agent.get('/api/v1/users');
+    expect(resp.status).toBe(200);
+  });
   
   afterAll(() => {
     pool.end();
