f4050bc6606c8ed5a93e47f186a87a96bc553d9b
Colter Garrison <102547626+Colter-Garrison@users.noreply.github.com>
2022-08-16 12:59:01 -0700

    403 test passing FINALLY! Had a TA help me and delete my loging and regoster function and define the agent in the test.

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,7 +2,6 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
-const UserService = require('../lib/services/UserService');
 
 const testUser = {
   firstName: 'Tiesto',
@@ -11,24 +10,15 @@ const testUser = {
   password: 'asdf',
 };
 
-const registerAndLogin = async (userProps = {}) => {
-  const password = userProps.password ?? testUser.password;
-  const agent = request.agent(app);
-  const user = await UserService.create({ ...testUser, userProps });
-  const { email } = user;
-  await (await agent.post('/api/v1/users/sessions')).setEncoding({ email, password });
-  return [agent, user];
-};
-
 describe('yawp POST, GET, and DELETE route tests', () => {
   beforeEach(() => {
     return setup(pool);
   });
-
+  
   it('POST should create a new user', async () => {
     const resp = await request(app).post('/api/v1/users').send(testUser);
     const { firstName, lastName, email } = testUser;
-
+    
     expect(resp.body).toEqual({
       id: expect.any(String),
       firstName,
@@ -36,17 +26,22 @@ describe('yawp POST, GET, and DELETE route tests', () => {
       email,
     });
   });
-
+  
   it('POST should sign in an existing user', async () => {
     await request(app).post('/api/v1/users').send(testUser);
     const resp = await request(app).post('/api/v1/users/sessions').send({ email: 'testemail@test.com', password: 'asdf' });
     expect(resp.status).toBe(200);
   });
-
-  it('GET should return a list of users for admins', async () => {
-    const [agent] = await registerAndLogin();
-    const resp = await agent.get('/api/v1/users');
-    expect(resp.status).toBe(200);
+  
+  it('should return a 403 when signed in but not admin when listing all users', async () => {
+    const agent = request.agent(app);
+    await agent.post('/api/v1/users').send(testUser);
+    await agent.post('/api/v1/users/sessions').send({ email: 'testemail@test.com', password: 'asdf' });
+    const res = await agent.get('/api/v1/users');
+    expect(res.body).toEqual({
+      message: 'You do not have access to view this page',
+      status: 403,
+    });
   });
   
   afterAll(() => {
